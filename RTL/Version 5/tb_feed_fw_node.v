27fe6b840d4a812336c5bc4776fef3ad81f017d2
18521449 <80138548+18521449@users.noreply.github.com>
2022-04-20 22:04:26 +0700

    Update tb_feed_fw_node.v

--- RTL/Version 5/tb_feed_fw_node.v
+++ RTL/Version 5/tb_feed_fw_node.v
@@ -2,31 +2,40 @@
 module tb_feed_fw_node();
 
 parameter DATA_WIDTH =32;
-parameter NUMBER_OF_INPUT_NODE = 3;
+parameter NUMBER_OF_INPUT_NODE = 33;
 parameter LEAKYRELU_ENABLE = 1'b1;
 parameter [DATA_WIDTH-1:0]  ALPHA = 32'h3DCCCCCD;
+parameter DATA_FILE = "data.txt";
+parameter WEIGHT_FILE = "weight.txt";
 parameter k =5;
 
 reg 							clk;
 reg 							rst_n;
 reg 							i_valid;
-reg 		[DATA_WIDTH-1:0] 	i_weight;
-reg			[DATA_WIDTH-1:0] 	i_data;
+// reg 		[DATA_WIDTH-1:0] 	i_weight;
+// reg		[DATA_WIDTH-1:0] 	i_data;
 wire		[DATA_WIDTH-1:0] 	o_data;
 wire	 						o_valid;
+
+reg [DATA_WIDTH-1:0] data 		[NUMBER_OF_INPUT_NODE-1:0];
+reg [DATA_WIDTH-1:0] weight 	[NUMBER_OF_INPUT_NODE-1:0];
+reg [5:0] counter;
 
 initial begin
 	clk <= 0;
 	rst_n <= 1;
 	i_valid <= 0;
+	$readmemh(DATA_FILE, data);
+	$readmemh(WEIGHT_FILE, weight);
+	counter <= 0;
 	#k#k 	i_valid <= 1;
-			i_weight <= 32'h40A66666;
-			i_data <= 32'h40500000;
-	#k#k 	i_weight <= 32'h4089999A;
-			i_data <= 32'h40300000;
-	#k#k	i_weight <= 32'h3E19999A;
-			i_data <= 32'h3F800000;
-	#k#k	i_valid <= 0;
+			// i_weight <= 32'h40A66666;
+			// i_data <= 32'h40500000;
+	// #k#k 	i_weight <= 32'h4089999A;
+			// i_data <= 32'h40300000;
+	// #k#k	i_weight <= 32'h3E19999A;
+			// i_data <= 32'h3F800000;
+	// #k#k	i_valid <= 0;
 end
 
 feed_forward_node
@@ -39,13 +48,19 @@ feed_forward_node
 	(	.clk					(clk),
 		.rst_n					(rst_n),
 		.i_valid				(i_valid),
-		.i_weight				(i_weight),
-		.i_data					(i_data),
+		.i_weight				(weight[counter]),
+		.i_data					(data[counter]),
 		.o_data 				(o_data),
 		.o_valid				(o_valid)
 	);
 
 always @(posedge clk) begin
+	if (i_valid) begin
+		counter <= counter + 1;
+	end
+	if (counter == NUMBER_OF_INPUT_NODE-1) begin
+		i_valid <= 0;
+	end
 	if (o_valid) begin
 		$display("%h", o_data);
 		#k#k#k#k $finish;
@@ -57,7 +72,3 @@ always @(*) begin
 end
 
 endmodule
-
-
-
-	
