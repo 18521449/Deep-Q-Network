6930923d1c301fabcd95e0101d98e4fe75d016dc
18521449 <80138548+18521449@users.noreply.github.com>
2022-06-18 21:56:21 +0700

    Update random_galois_23bit.v

--- RTL/Version 13/random_galois_23bit.v
+++ RTL/Version 13/random_galois_23bit.v
@@ -10,11 +10,11 @@ module random_galois_23bit
 	input 						clk;
 	input 						rst_n;
 	input 						i_enable;
-	output		[23:1] 			o_random_data;
+	output		[22:0] 			o_random_data;
 	//----------------------------------------//
 	
 	wire 	taps, Q_x;
-	reg 	[23:1] Q_next, Q_reg;
+	reg 	[22:0] Q_reg;
 	
 	initial begin
 		Q_reg <= 'd65;
@@ -22,9 +22,9 @@ module random_galois_23bit
 	
 	assign o_random_data = Q_reg;
 	
-	assign taps = Q_reg[1];
+	assign taps = Q_reg[0];
 	
-	assign Q_x = Q_reg[1] ^ Q_reg[6];
+	assign Q_x = Q_reg[0] ^ Q_reg[5];
 	
 	always @(posedge clk or negedge rst_n) begin
 		if (!rst_n) begin
@@ -32,12 +32,8 @@ module random_galois_23bit
 		end
 		else begin
 			if (i_enable) 
-				Q_reg <= Q_next;
+				Q_reg <= {taps, Q_reg[22:5], Q_x, Q_reg[3:1]};
 		end
-	end
-	
-	always @(taps or Q_reg) begin
-		Q_next <= {taps, Q_reg[23:6], Q_x, Q_reg[4:2]};
 	end
 	
 endmodule
